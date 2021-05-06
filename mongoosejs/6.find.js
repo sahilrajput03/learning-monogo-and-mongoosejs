f19c9fac59ec62a30f099a6bb2e448615851ac9d
Sahil Rajput <sahilrajput03@gmail.com>
2021-05-06 15:05:28 +0530

    update. ~ sahil

--- mongoosejs/6.find.js
+++ mongoosejs/6.find.js
@@ -1,11 +1,14 @@
 const {personModel, log} = require('./0.mongoclient')
 
-const useAsyncFunc = 1 // 0 = false, 1 = true
+const useAsyncFunc = 0 // 0 = false, 1 = true
 
 const asyncFunc = async () => {
 	log('::Using async/await for fetching:')
 
 	let reply = await personModel.find({})
+	// For populating a property:
+	// let reply = await personModel.find({}).populate('somefield')
+
 	log('::get.js', {reply})
 }
 
@@ -15,6 +18,7 @@ const syncFunc = () => {
 	personModel.find({}, function (err, reply) {
 		if (err) return console.error(err)
 		log('::get.js', {reply})
+		// To know about synchronous mode of populate, refer: 8.findById-populate.js
 	})
 }
 
