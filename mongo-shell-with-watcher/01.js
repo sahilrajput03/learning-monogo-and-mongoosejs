200fbba3ae8f55acf5b493d918e4bf066423fadc
Sahil Rajput <sahilrajput03@gmail.com>
2025-06-11 18:36:06 +0530

    Update.

--- mongo-shell-with-watcher/01.js
+++ mongo-shell-with-watcher/01.js
@@ -3,38 +3,48 @@
 // Refer docs page to know the Javacript Equivalents of cli instructions: https://www.mongodb.com/docs/v5.0/tutorial/write-scripts-for-the-mongo-shell/
 
 db = db.getSiblingDB('mydb');
-// print(db) // mydb
-// print(db.getUsers) // get user objects
-// print(db.getCollectionNames()) // postman,posts
+print('🚀Database name?', db); // mydb
+// print(db.getUsers); // get user objects
 
+db.posts.drop();
+
+print('\n🚀Inserting one document in posts collection:');
+// * Below throws - DeprecationWarning: Collection.insert() is deprecated. Use insertOne, insertMany, or bulkWrite.
 // Learn: print() methods prints the info from the operation ~Sahil
-// print(
-// 	db.posts.insert({
-// 		title: 'Post One',
-// 		body: 'Body of post one',
-// 		category: 'News',
-// 		likes: 4,
-// 		tags: ['news', 'events'],
-// 		user: {
-// 			name: 'John Doe',
-// 			status: 'author',
-// 		},
-// 		data: Date(),
-// 	})
-// )
+print(
+    db.posts.insert({
+        title: 'Post One',
+        body: 'Body of post one',
+        category: 'News',
+        likes: 4,
+        tags: ['news', 'events'],
+        user: {
+            name: 'John Doe',
+            status: 'author',
+        },
+        data: Date(),
+    })
+);
 
 let myCursor = db.posts.find();
-// myCursor.forEach(printjson); // Works like a charm. Src: https://www.mongodb.com/docs/manual/reference/method/db.collection.find/
+print('\n🚀Printing each document in posts collection:');
+myCursor.forEach(printjson); // Works like a charm. Src: https://www.mongodb.com/docs/manual/reference/method/db.collection.find/
 
 
-// db.createCollection('postman')
+db.createCollection('postman');
 
-// print(
-// 	db.posts.insert({
-// 		postmanName: 'Postman1',
-// 		postmanBody: 'PostmanBody1',
-// 	})
-// )
+print('\n🚀Inserting one document in postman collection:');
+print(
+    db.posts.insert({
+        postmanName: 'Postman1',
+        postmanBody: 'PostmanBody1',
+    })
+);
 
-// print('hello world')
-// print([1,2,3].map(k => k+1)) // works like a charm
+
+print('\n🚀Using map fuction:');
+print([1, 2, 3].map(k => k + 1)); // works like a charm
+
+
+print('\n🚀Print all collection names in "mydb" database:');
+print(db.getCollectionNames()); // [ 'persons', 'posts' ]
