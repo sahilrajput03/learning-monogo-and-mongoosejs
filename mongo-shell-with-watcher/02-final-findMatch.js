18e8abb4f1d4b25e2076b61dab6e161838214db2
Sahil Rajput <sahilrajput03@gmail.com>
2025-06-11 18:09:56 +0530

    Update.

--- mongo-shell-with-watcher/02-final-findMatch.js
+++ mongo-shell-with-watcher/02-final-findMatch.js
@@ -1,24 +1,24 @@
 // @ts-nocheck
-db = db.getSiblingDB('mydb')
+db = db.getSiblingDB('mydb');
 
-let currentUserId = 100
+let currentUserId = 100;
 
 // Get matches
-let query1 = db.datingLikes.find({ to: currentUserId })
-let list = query1.toArray().map(item => item.from)
-let matches_query2 = db.datingLikes.find({ to: { "$in": list}})
+let query1 = db.datingLikes.find({ to: currentUserId });
+let list = query1.toArray().map(item => item.from);
+let matches_query2 = db.datingLikes.find({ to: { "$in": list } });
 
-print('Matches:') // Users who likes currentUser and currentUser also likes them:
+print('Matches:'); // Users who likes currentUser and currentUser also likes them:
 matches_query2.forEach(printjson);
 
 // let matches = db.datingLikes.aggregate([{$match: { to: currentUserId }}, {match: {from : {"$in": }}}]) // TODO Optimize: try2
 // let matches = db.datingLikes.aggregate([{$match: { $or: filterLikedCurrentUser }}, ??query?? ]) // TODO optimizing for later
 
 // Get Time taken for the query
-printjson('Time took: query1 (ms): ' + query1.explain('executionStats').executionStats.executionTimeMillis)
-printjson('Time took: query2 (ms): ' + matches_query2.explain('executionStats').executionStats.executionTimeMillis)
-printjson('Docs Examined: query1: ' + query1.explain('executionStats').executionStats.totalDocsExamined)
-printjson('Docs Examined: query2: ' + matches_query2.explain('executionStats').executionStats.totalDocsExamined)
+printjson('Time took: query1 (ms): ' + query1.explain('executionStats').executionStats.executionTimeMillis);
+printjson('Time took: query2 (ms): ' + matches_query2.explain('executionStats').executionStats.executionTimeMillis);
+printjson('Docs Examined: query1: ' + query1.explain('executionStats').executionStats.totalDocsExamined);
+printjson('Docs Examined: query2: ' + matches_query2.explain('executionStats').executionStats.totalDocsExamined);
 
 // PRINT ALL LIKES DOCS
 // db.datingLikes.find().forEach(printjson)
--- mongo-shell-with-watcher/watch.sh
+++ mongo-shell-with-watcher/watch.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+
+# Learn: As of MongoDB 5.0+, `mongo` shell is deprecated. You probably
+# 		have mongosh instead.
+
+# Usage: `watch.sh sh-1`
+
 if [ $# -eq 0 ]; then
 	echo "Please give the file as argument."
 else
-	# Learn: As of MongoDB 5.0+, `mongo` shell is deprecated. You probably
-	# 		have mongosh instead.
 	nodemon -w "$@" -x "mongosh --quiet localhost:27017/mydb < $@"
 	# nodemon -w "$@" -x "mongo localhost:27017/mydb < $@"
 fi
