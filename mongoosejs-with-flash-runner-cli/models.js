0d1b0f948f3fe302d3bf019624e594690a99d7b2
Sahil Rajput <sahilrajput03@gmail.com>
2025-06-16 18:28:27 +0530

    Update.

--- mongoosejs-with-flash-runner-cli/models.js
+++ mongoosejs-with-flash-runner-cli/models.js
@@ -1,12 +1,20 @@
+const mongoose = require('mongoose');
 const { Schema, model } = require('mongoose');
 
-//* You should use pluras names, otherwise mongodb will itself change the name to plurals.
-const personsCollectionName = 'persons';
-const gadgetsCollectionName = 'gadgets';
-const carsCollectionName = 'cars';
+
+// Note: We use pluras names, otherwise mongodb will itself change the
+// 		name to plurals and we might know only when changes are saved to
+// 		db.
+// @ts-ignore
+const personsCollectionName = mongoose.pluralize()('persons');
+// @ts-ignore
+const gadgetsCollectionName = mongoose.pluralize()('gadgets');
+// @ts-ignore
+const carsCollectionName = mongoose.pluralize()('cars');
 
 let personSchema = new Schema(
 	{
+		// Using "ref" tells moongoose to join gadget docs from 'gadgetsCollectionName' collection.
 		gadgetList: [{ type: Schema.Types.ObjectId, ref: gadgetsCollectionName }],
 		favouriteGadget: { type: Schema.Types.ObjectId, ref: gadgetsCollectionName },
 	},
@@ -14,22 +22,11 @@ let personSchema = new Schema(
 	// 		irrespective of the schema definition. (https://stackoverflow.com/a/12389168)
 	{ strict: false }
 );
+const PersonModel = model(personsCollectionName, personSchema);
 
-// Using above type of ref tells moongoose to treat, gadgets field as
-// 		join from `GADGET_COLLECTION_NAME` collection.
-const PersonModel = model(
-	personsCollectionName, //LEARN: Model name(if third param is omiited, mongoose will pluralize `modelName` to get a `collectionName`.)
-	personSchema,
-	personsCollectionName //LEARN: Collection name(optional param but IMPORTANT) ensures that mongodb doesn't alter our name to pluras or sht thigs on itself.
-);
 
 let gadgetSchema = new Schema({ deviceName: String }, { strict: false });
-// ? Using above type of ref tells moongoose to treat, gadgets field as join from 'gadgetcollection' collection.
-const gadgetModel = model(
-	gadgetsCollectionName,
-	gadgetSchema,
-	gadgetsCollectionName //Using third parameter ensures that mongoose will only use this name as collection name, and won't pluralize it. Yikes!
-);
+const gadgetModel = model(gadgetsCollectionName, gadgetSchema);
 
 const carSchema = new Schema({
 	carName: {
@@ -47,25 +44,20 @@ const carSchema = new Schema({
 				`${props.value} is not allowed. Only audi and bmw cars are allowed.`,
 		},
 	},
-	// Email id to be unique (validates on saving any doc).
 	email: {
 		type: String,
-		unique: true, // `email` must be unique
+		unique: true, // `email` must be unique (validates on saving any doc)
 	},
 	report: [{
 		_id: Schema.Types.ObjectId,
 		reason: String
 	}]
 });
+const CarModel = model(carsCollectionName, carSchema);
 // carSchema.index(
 // 	{ email: 1 }
 // );
 
-const CarModel = model(
-	carsCollectionName,
-	carSchema,
-	carsCollectionName //Using third parameter ensures that mongoose will only use this name as collection name, and won't pluralize it. Yikes!
-);
 
 module.exports = {
 	PersonModel,
